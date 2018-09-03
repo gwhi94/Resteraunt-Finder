18251b2f98a1d77ecc4d3fb4c93e873d938d8c7d
George Whiting <gwhi94@gmail.com>
2018-09-03 20:47:33 +0100

    Removed commented code and deleted unused file

--- sideBarPop.js
+++ sideBarPop.js
@@ -83,12 +83,3 @@ addReview(filter1,filter2);
 }
 
 
-/*function mapControl(filter1,filter2) { //function to detect map change and handle assoc
-                                        //events
-
-google.maps.event.addListener(map,'click', function(event) {formControl(event)});
-
-
-google.maps.event.addListener(map, 'dragend', function() {sideBarPop(filter1,filter2),addReview(filter1,filter2)} );
-
-}*/
